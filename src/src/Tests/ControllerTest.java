09fb46cf3b495fe9ca3ce19c0f328a1fa59371a8
lucas <minibba@protonmail.com>
2024-05-15 09:09:25 +0200

    ControllerTest

---
+++ src/src/Tests/ControllerTest.java
@@ -0,0 +1,56 @@
+package Tests;
+import application.controller.Controller;
+import application.model.Aftapning;
+import application.model.Destillat;
+import application.model.Destillering;
+import application.model.Fad;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void createDestilleringTest() {
+        LocalDate startDato = LocalDate.now();
+        LocalDate slutDato = LocalDate.now().plusDays(1);
+        Destillering destillering = Controller.createDestillering(startDato, slutDato, 1000, "Byg", 500, 40.0, "Ingen kommentar", "Tørv");
+        assertEquals(startDato, destillering.getStartDato());
+        assertEquals(slutDato, destillering.getSlutDato());
+    }
+
+    @Test
+    void createDestilleringUgyldigeDatoer() {
+        LocalDate startDato = LocalDate.now();
+        LocalDate slutDato = LocalDate.now().minusDays(1);
+        assertThrows(IllegalArgumentException.class, () -> Controller.createDestillering(startDato, slutDato, 1000, "Byg", 500, 40.0, "Ingen kommentar", "Tørv"));
+    }
+
+    @Test
+    void createFadTest() {
+        Fad fad = Controller.createFad("fadHistorie", "tidligereBrug", 1, "koebssted", "fadNavn", 100);
+        assertEquals("History", fad.getFadHistore());
+        assertEquals("Previous Use", fad.getTidligereBrug());
+    }
+
+    @Test
+    void createFadUgyldigKapacitet() {
+        assertThrows(IllegalArgumentException.class, () -> Controller.createFad("", "tidligereBrug", 1, "koebssted", "fadNavn", -100));
+    }
+
+    @Test
+    void createAftapningTest() {
+        Fad fad = new Fad("fadHistorie", "tidligereBrug", 1, "koebssted", "fadNavn", 100);
+        ArrayList<Destillat> destillat = new ArrayList<>();
+        Aftapning aftapning = Controller.createAftapning(fad, destillat, 50, LocalDate.now());
+        assertEquals(50, aftapning.getLiter());
+    }
+
+    @Test
+    void createAftapningUgyldigLiter() {
+        Fad fad = new Fad("fadHistorie", "tidligereBrug", 1, "koebssted", "fadNavn", 100);
+        ArrayList<Destillat> destillat = new ArrayList<>();
+        assertThrows(IllegalArgumentException.class, () -> Controller.createAftapning(fad, destillat, 150, LocalDate.now()));
+    }
+}
