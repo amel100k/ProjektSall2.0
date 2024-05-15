c11d05ccaeb202e1394be76342ee3d7886a5a17b
amel100k <eaa23ema@students.eaaa.dk>
2024-05-15 10:59:55 +0200

    hh

--- src/src/application/model/Produkt.java
+++ src/src/application/model/Produkt.java
@@ -6,6 +6,7 @@ public class Produkt {
     private ArrayList<Aftapning> aftapninger;
     private int kapacitetILiter;
     private double alkoholprocent;
+    private int bertil;
 
     public Produkt(ArrayList<Aftapning> aftapninger, double alkoholprocent) {
         this.aftapninger = aftapninger;
